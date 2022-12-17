4077b954a8002366c98a9cef3e7bd307fef10f90
Nikos Patsiatzis <n.patsiatzis@gmail.com>
2022-12-17 12:11:37 +0200

    added a CoCoTB-test simulation flow to run the tests across a range of values for generic parameters

--- testbench.py
+++ testbench.py
@@ -7,7 +7,7 @@ from cocotb_coverage.coverage import CoverCross,CoverPoint,coverage_db
 from cocotb.queue import Queue,QueueEmpty,QueueFull
 
 
-covered_number = []
+covered_value = []
 g_width = int(cocotb.top.g_width)
 g_depth = int(cocotb.top.g_depth)
 fifo = []
@@ -17,6 +17,20 @@ fifo_rd = []
 wr = 0 
 rd = 0 
 data = 0 
+
+
+# #Callback functions to capture the bin content showing
+def notify_full():
+	global full
+	full = True
+
+
+# at_least = value is superfluous, just shows how you can determine the amount of times that
+# a bin must be hit to considered covered
+@CoverPoint("top.gray",xf = lambda x : x, bins = list(range(2**g_width)), at_least=1)
+def number_cover(x):
+	covered_value.append(x)
+	
 
 async def reset(dut,cycles=1):
 	dut.i_arstnW.value = 0
@@ -79,13 +93,11 @@ async def test_underflow(dut):
 	cocotb.start_soon(Clock(dut.i_clkR, 20, units="ns").start())
 	await reset(dut,5)
 
-	wr = 1 
-	rd = 0 
+	# ready_A = 1 
 	data = random.randint(0,2**g_width-1)
 
-	dut.i_wren.value = wr 
-	dut.i_ren.value = rd 
-	dut.i_dataW.value = data 
+	# dut.i_ready_A.value = ready_A 
+	# dut.i_data_A.value = data 
 
 	await RisingEdge(dut.i_clkW)
 	for i in range (2):
@@ -99,8 +111,8 @@ async def test_underflow(dut):
 	dut.i_wren.value = wr 
 	dut.i_ren.value = rd 
 	await RisingEdge(dut.i_clkR)
-	for i in range(2*g_depth + 2):
+	for i in range(2**g_depth + 2):
 		await RisingEdge(dut.i_clkR)
 
-	assert not (dut.o_empty != 1),"Empty flag falsely not set!"
+	assert not (dut.o_empty.value != 1),"Empty flag falsely not set!"
 	assert not (dut.addr_r.value != dut.addr_w.value),"Overflow issue detected!"
